1e38de8e3b38e79a0216267f7468930ddfa67192
karthik-sr4dx <karthik@sr4dx.com>
2026-01-12 19:40:05 +0530

    completed cleaning and chunking

--- src/common/queues/injection-queue/injection-queue.module.ts
+++ src/common/queues/injection-queue/injection-queue.module.ts
@@ -4,14 +4,15 @@ import { InjectionProcessor } from './injection.processor';
 import { Document } from 'src/entities/document.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DocumentPages } from 'src/entities/document.pages.entity';
-
+import { ProcessProcessor } from './process.processor';
+import { DocumentChunk } from 'src/entities/document.chunks.entity';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Document, DocumentPages]),
+    TypeOrmModule.forFeature([Document, DocumentPages, DocumentChunk]),
     BullModule.registerQueue({ name: 'injectionQueue' }),
   ],
-  providers: [InjectionProcessor],
+  providers: [InjectionProcessor , ProcessProcessor],
   exports: [BullModule],
 })
 export class InjectionQueueModule {}
--- src/entities/document.chunks.entity.ts
+++ src/entities/document.chunks.entity.ts
@@ -1,9 +1,68 @@
-// document_chunks
-// - id
-// - document_id
-// - page_start
-// - page_end
-// - chunk_text
-// - embedding (pgvector)
-// - section_title
-// - created_at
+import { Column, Entity, PrimaryGeneratedColumn, Index } from 'typeorm';
+
+export enum ChunkStatus {
+  PENDING = 'PENDING',
+  EMBEDDED = 'EMBEDDED',
+  FAILED = 'FAILED',
+}
+
+@Entity({ name: 'document_chunks' })
+@Index('idx_chunks_document', ['documentId'])
+@Index('idx_chunks_doc_chunk', ['documentId', 'chunkIndex'])
+export class DocumentChunk {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ name: 'document_id', type: 'int' })
+  documentId: number;
+
+  @Column({ name: 'page_start', type: 'int' })
+  pageStart: number;
+
+  @Column({ name: 'page_end', type: 'int' })
+  pageEnd: number;
+
+  @Column({ name: 'chunk_index', type: 'int' })
+  chunkIndex: number;
+
+  @Column({ type: 'jsonb', nullable: true })
+  sectionPath: string[]; 
+  // ["Authentication", "JWT Tokens"]
+
+  @Column({ name: 'section_title', type: 'text', nullable: true })
+  sectionTitle: string;
+
+  @Column({ name: 'chunk_text', type: 'text' })
+  chunkText: string;
+
+  // Critical for RAG pipeline control
+  @Column({ name: 'token_count', type: 'int' , nullable : true })
+  tokenCount: number;
+
+  /**
+   * pgvector column
+   * Must be created as: embedding vector(1536)
+   */
+  @Column({ name: 'embedding', type: 'vector', length: 1536, nullable: true })
+  embedding: number[];
+
+  @Column({ name: 'status', type: 'varchar', default: ChunkStatus.PENDING })
+  status: ChunkStatus;
+
+  @Column({
+    name: 'created_on',
+    type: 'timestamp',
+    default: () => 'CURRENT_TIMESTAMP',
+  })
+  createdOn: Date;
+
+  @Column({ name: 'created_by', type: 'int', nullable: true })
+  createdBy: number;
+
+  @Column({ name: 'updated_on', type: 'timestamp', nullable: true })
+  updatedOn: Date;
+
+  @Column({ name: 'updated_by', type: 'int', nullable: true })
+  updatedBy: number;
+}
